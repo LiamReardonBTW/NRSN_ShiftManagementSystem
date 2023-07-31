1dbde2ddaa8d746a223dd55347efcbdcec88edae
LiamReardonBTW <liambreardon@gmail.com>
2023-07-31 11:54:28 +1000

    Added inactive user to populate users migration
    - Added a default inactive user account for testing purposes

--- database/migrations/2023_07_25_062039_populate_users_table.php
+++ database/migrations/2023_07_25_062039_populate_users_table.php
@@ -12,9 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         DB::table('users')->insert([
-            ['id' => 1, 'name' => 'admin', 'email' => 'admin@nrsn.com', 'password' => bcrypt('root1234'), 'role_as' => 1],
-            ['id' => 2, 'name' => 'manager', 'email' => 'manager@nrsn.com', 'password' => bcrypt('root1234'), 'role_as' => 2],
-            ['id' => 3, 'name' => 'worker', 'email' => 'worker@nrsn.com', 'password' => bcrypt('root1234'), 'role_as' => 3]
+            ['id' => 1, 'name' => 'admin', 'email' => 'admin@nrsn.com', 'password' => bcrypt('root1234'), 'role_as' => 1, 'active_status' => '1'],
+            ['id' => 2, 'name' => 'manager', 'email' => 'manager@nrsn.com', 'password' => bcrypt('root1234'), 'role_as' => 2, 'active_status' => '1'],
+            ['id' => 3, 'name' => 'worker', 'email' => 'worker@nrsn.com', 'password' => bcrypt('root1234'), 'role_as' => 3, 'active_status' => '1'],
+            ['id' => 4, 'name' => 'inactive', 'email' => 'inactive@nrsn.com', 'password' => bcrypt('root1234'), 'role_as' => 3, 'active_status' => '0'],
         ]);
     }
 
